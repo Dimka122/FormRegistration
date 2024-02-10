12ea0498e5fca828a89220f6ad21f4cac33b5c62
Dimka122 <kassindima1@gmail.com>
2024-02-10 03:09:22 +0200

    new project MVC

---
+++ Forma Registr/Forma Registr/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Forma_Registr.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
