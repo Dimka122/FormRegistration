845f7867b294097c1118edd44ea7dc860fc4fe5c
Dimka122 <kassindima1@gmail.com>
2024-02-11 17:29:03 +0200

    Создание регистрации и входа пользователя с помощью Web API и ReactJS

--- Forma Registr/Forma Registr/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Forma_Registr.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ RegistrationForms/RegistrationForms/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RegistrationForms.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
