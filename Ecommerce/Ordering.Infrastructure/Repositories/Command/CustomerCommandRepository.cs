eab144a5e7a78d851de82bbd5648b647fee117fb
Dimka122 <kassindima1@gmail.com>
2024-02-16 12:25:27 +0200

    репозиторий команд и запросов в проекте Ordering.Infrastructure

---
+++ Ecommerce/Ordering.Infrastructure/Repositories/Command/CustomerCommandRepository.cs
@@ -0,0 +1,20 @@
+﻿using Ordering.Core.Entities;
+using Ordering.Core.Repositories.Command;
+using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Repositories.Command.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories.Command
+{
+    public class CustomerCommandRepository : CommandRepository<Customer>, ICustomerCommandRepository
+    {
+        public CustomerCommandRepository(OrderingContext context) : base(context)
+        {
+
+        }
+    }
+}
