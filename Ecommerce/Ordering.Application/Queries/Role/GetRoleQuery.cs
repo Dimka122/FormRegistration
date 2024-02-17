19f9c1b106192aaa05672554783d64991ad14b84
Dimka122 <kassindima1@gmail.com>
2024-02-17 07:32:24 +0200

    Создание классов и обработчиков запросов ролей в папке Ordering.Application/Queries/Role

---
+++ Ecommerce/Ordering.Application/Queries/Role/GetRoleQuery.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Ordering.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Queries.Role
+{
+    public class GetRoleQuery : IRequest<IList<RoleResponseDTO>>
+    {
+
+    }
+
+    public class GetRoleQueryHandler : IRequestHandler<GetRoleQuery, IList<RoleResponseDTO>>
+    {
+        private readonly IIdentityService _identityService;
+
+        public GetRoleQueryHandler(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+        public async Task<IList<RoleResponseDTO>> Handle(GetRoleQuery request, CancellationToken cancellationToken)
+        {
+            var roles = await _identityService.GetRolesAsync();
+            return roles.Select(role => new RoleResponseDTO() { Id = role.id, RoleName = role.roleName }).ToList();
+        }
+    }
+}
